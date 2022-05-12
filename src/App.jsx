9e70611ecb3aa80540bc6acfad79169570f74f57
parzivalElijah <aithammouk40@gmail.com>
2022-05-12 10:33:54 +0100

    turned class components into functional ones

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./components/SearchBar";
 import youtube from "./api/youtube";
 import VideoList from "./components/Videos";
 import VideoDetails from "./components/VideoDetails";
 
-class App extends React.Component {
-  state = { thumbs: [], selectedVideo: null };
-  componentDidMount() {
-    this.onSearchSubmit("hello");
-  }
-  onSearchSubmit = async (term) => {
+const App = () => {
+  const [thumbs, setThumbs] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  useEffect(() => {
+    onSearchSubmit("hello");
+  }, []);
+
+  const onSearchSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
-    this.setState({
-      thumbs: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
+    setThumbs(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   };
-  onVideoSelect = (thumb) => {
-    this.setState({ selectedVideo: thumb });
-  };
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: "20px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <div className="ui grid" style={{ marginTop: "20px" }}>
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetails thumb={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                thumbs={this.state.thumbs}
-              />
-            </div>
+
+  return (
+    <div className="ui container" style={{ marginTop: "20px" }}>
+      <SearchBar onSubmit={onSearchSubmit} />
+      <div className="ui grid" style={{ marginTop: "20px" }}>
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetails thumb={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={setSelectedVideo} thumbs={thumbs} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,29 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./search.module.css";
-class SearchBar extends React.Component {
-  state = { term: "" };
-  onFormSubmit = (e) => {
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const onFormSubmit = (e) => {
     e.preventDefault();
-    this.props.onSubmit(this.state.term);
+    onSubmit(term);
   };
 
-  render() {
-    return (
-      <div>
-        <form className="form" onSubmit={this.onFormSubmit}>
-          <div className={styles.field}>
-            <input
-              className={styles.inputText}
-              type="search"
-              value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })}
-              placeholder="search video"
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form className="form" onSubmit={onFormSubmit}>
+        <div className={styles.field}>
+          <input
+            className={styles.inputText}
+            type="search"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            placeholder="search video"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
